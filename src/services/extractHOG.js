6b426f994aa2733dd8ec3e2ce91024cb0a8e0090
khuongnd99 <khuongnd@edunautix.com>
2021-04-28 20:03:47 +0700

    resize img

--- src/services/extractHOG.js
+++ src/services/extractHOG.js
@@ -3,7 +3,7 @@ const hog = require('hog-features');
 
 async function extractHOG(file) {
   const image = await Image.load(file);
-  const descriptor = hog.extractHOG(image, {cellSize: 32});
+  const descriptor = hog.extractHOG(image, {cellSize: 16});
   return descriptor;
 }
 module.exports = extractHOG;
